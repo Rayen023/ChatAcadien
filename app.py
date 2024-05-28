6a277c9ddaa601c596c036c3645c9d6d9c5a5378
Rayen023 <rayenghali02@gmail.com>
2024-05-28 17:20:17 -0300

    ``` Enhance chat and retrieval logic, improve feedback logging
    Updated chat application to incorporate new model integrations,
    enhancing AI response retrieval and processing. Added logging
    functionalities to capture user feedback, and enabled chat history
    management. Clarified and streamlined code for maintainability.
    
    Updated dependencies in requirements to support new features.
    
    Addresses feedback improvements and retrieval efficiency.
    ```

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
+import os
+from datetime import datetime
+import logging
+import json
+
 import streamlit as st
-from langchain_community.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
-from langchain.prompts import (
-    MessagesPlaceholder,
-    ChatPromptTemplate,
-)
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+import tiktoken
 
-
-## Statefully manage chat history ###
-from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain.globals import set_debug
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.chat_models import ChatOpenAI
+from langchain_cohere import CohereEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.tools.retriever import create_retriever_tool
+from langchain.memory import ConversationBufferMemory
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+from langchain.agents import AgentExecutor
+from langchain.agents.format_scratchpad import format_to_openai_functions
+from langchain.tools.render import format_tool_to_openai_function
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
 
 
 # App title
@@ -28,164 +33,134 @@ free_api_key = (
 
 with st.sidebar:
 
-    ms = st.session_state
-    if "themes" not in ms:
-        ms.themes = {
-            "current_theme": "light",
-            "refreshed": True,
-            "light": {
-                "theme.base": "dark",
-                # "theme.backgroundColor": "black",
-                # "theme.primaryColor": "#c98bdb",
-                # "theme.secondaryBackgroundColor": "#5591f5",
-                "theme.textColor": "white",
-                "theme.textColor": "white",
-                "button_face": "🌘",
-            },
-            "dark": {
-                "theme.base": "light",
-                # "theme.backgroundColor": "white",
-                # "theme.primaryColor": "#5591f5",
-                # "theme.secondaryBackgroundColor": "#82E1D7",
-                "theme.textColor": "#0a1464",
-                "button_face": "🌖",
-            },
-        }
-
-    def ChangeTheme():
-        previous_theme = ms.themes["current_theme"]
-        tdict = (
-            ms.themes["light"]
-            if ms.themes["current_theme"] == "light"
-            else ms.themes["dark"]
-        )
-        for vkey, vval in tdict.items():
-            if vkey.startswith("theme"):
-                st._config.set_option(vkey, vval)
-
-        ms.themes["refreshed"] = False
-        if previous_theme == "dark":
-            ms.themes["current_theme"] = "light"
-        elif previous_theme == "light":
-            ms.themes["current_theme"] = "dark"
-
-    btn_face = (
-        ms.themes["light"]["button_face"]
-        if ms.themes["current_theme"] == "light"
-        else ms.themes["dark"]["button_face"]
-    )
-    st.button(btn_face, on_click=ChangeTheme)
-
-    if ms.themes["refreshed"] == False:
-        ms.themes["refreshed"] = True
-        st.rerun()
-
-    st.title("ChatAcadien")
-    # st.subheader("Models and parameters")
-
-    # temperature = st.sidebar.slider("temperature", min_value=0.0, max_value=5.0, value=0.1, step=0.01)
-    # top_p = st.sidebar.slider("top_p", min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    # max_length = st.sidebar.slider("max_length", min_value=64, max_value=4096, value=512, step=8)
+    st.title("Chat Acadien")
 
 prompt = st.chat_input("Message ChatAcadien...")
 
-
-set_debug(True)
-
-store = {}
-session_id = "default"
+logger = logging.getLogger()
+logging.basicConfig(encoding="UTF-8", level=logging.INFO)
 
 
-def get_session_history(session_id: str) -> BaseChatMessageHistory:
-    if session_id not in store:
-        store[session_id] = StreamlitChatMessageHistory(key="chat_messages")
-    return store[session_id]
+def log_feedback(icon):
+    # We display a nice toast
+    st.toast("Thanks for your feedback!", icon=":material/thumb_up:")
+
+    # We retrieve the last question and answer
+    last_messages = json.dumps(st.session_state["messages"][-2:])
+
+    # We record the timestamp
+    activity = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ": "
+
+    # And include the messages
+    activity += "positive" if icon == "👍" else "negative"
+    activity += ": " + last_messages
+
+    # And log everything
+    logger.info(activity)
 
 
-class ChatOpenRouter(ChatOpenAI):
-    openai_api_base: str
-    openai_api_key: str
-    model_name: str
-
-    def __init__(
-        self,
-        model_name: str,
-        openai_api_key: str = free_api_key,
-        openai_api_base: str = "https://openrouter.ai/api/v1",
-        **kwargs,
-    ):
-        openai_api_key = openai_api_key
-        super().__init__(
-            openai_api_base=openai_api_base,
-            openai_api_key=openai_api_key,
-            model_name=model_name,
-            **kwargs,
-        )
-
-
-llm = ChatOpenRouter(
-    # model_name="meta-llama/llama-3-8b-instruct:free",
-    model_name="google/gemma-7b-it:free",
-    temperature=0.0,
+cohere_api_key = "mLce2Wncyd6Cpv4WEsAVuJElVTnKQA3aGL776eAI"
+embeddings = CohereEmbeddings(
+    model="embed-english-light-v3.0", cohere_api_key=cohere_api_key
 )
 
-qa_system_prompt = """You are an AI chatbot having a conversation with a human .\
-"""
-general_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", qa_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
+if os.path.exists("chromadb2"):
+    vectorstore = Chroma(persist_directory="./chromadb2", embedding_function=embeddings)
+
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 5},
 )
 
-general_chain = general_prompt | llm | StrOutputParser()
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import CrossEncoderReranker
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 
-conv_general_chain = RunnableWithMessageHistory(
-    general_chain,
-    get_session_history,
-    input_messages_key="input",
-    history_messages_key="chat_history",
-    # output_messages_key="answer",
+model = HuggingFaceCrossEncoder(model_name="BAAI/bge-reranker-base")
+compressor = CrossEncoderReranker(model=model, top_n=1)
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor, base_retriever=retriever
+)
+
+retriever_tool = create_retriever_tool(
+    compression_retriever,
+    "computer_vision_and_defect_detection_search",
+    "If question is related to computer vision or defect detection, you must use this tool. When using this tool, for the query key, pass a detailed answer of 100 words to be used for better retrieval search. ",
 )
 
 
-# @st.cache_resource(ttl=600, show_spinner=False)
-def conversational_chat(query, session_id):
-    response = conv_general_chain.stream(
-        {"input": query},
-        config={"configurable": {"session_id": session_id}},
-    )
-    return response
+os.environ["TAVILY_API_KEY"] = "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"
 
+search = TavilySearchResults(max_results=2)
+
+tools = [search, retriever_tool]
+
+history = ChatMessageHistory()
 
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Comment puisse-je vous aidez?"}
     ]
 
-# Display or clear chat messages
+functions = [format_tool_to_openai_function(f) for f in tools]
+model = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    api_key="sk-proj-tymuOuG4sXN4tDpj9GXGT3BlbkFJa66Eox35dIijdwkrHP9B",
+    temperature=0,
+    streaming=True,
+).bind(functions=functions)
+
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are helpful assistant"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Comment puisse-je vous aidez?"}
+    ]
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
+
+        if message["role"] == "user":
+            history.add_user_message(message["content"])
+        else:
+            history.add_ai_message(message["content"])
+
+
+memory = ConversationBufferMemory(
+    return_messages=True, memory_key="chat_history", chat_memory=history
+)
+
+agent_chain = (
+    RunnablePassthrough.assign(
+        agent_scratchpad=lambda x: format_to_openai_functions(x["intermediate_steps"])
+    )
+    | prompt_template
+    | model
+    | OpenAIFunctionsAgentOutputParser()
+)
+
+agent_executor = AgentExecutor(
+    agent=agent_chain, tools=tools, verbose=True, memory=memory
+)
+
+
+def rerun_last_question():
+    st.session_state["messages"].pop(-1)
 
 
 def clear_chat_history():  # TODO
     st.session_state.messages = [
         {"role": "assistant", "content": "Comment puisse-je vous aidez?"}
     ]
-    store[session_id].clear()
-
-
-st.sidebar.button("Clear Chat History", on_click=clear_chat_history)
-
-
-# @st.cache_resource()
-def generate_response(prompt_input):
-    if not prompt_input:
-        return
-    output = conversational_chat(str(prompt_input), session_id=session_id)
-    return output
 
 
 if prompt:
@@ -193,10 +168,77 @@ if prompt:
     with st.chat_message("user"):
         st.write(prompt)
 
+
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        response = generate_response(prompt)
-        if response:
-            msg_ = st.write_stream(response)
-    message = {"role": "assistant", "content": msg_}
+    st_callback = StreamlitCallbackHandler(
+        st.chat_message("assistant"),
+        expand_new_thoughts=True,
+        collapse_completed_thoughts=False,
+        max_thought_containers=0,
+    )
+    response = agent_executor.invoke(
+        {"input": st.session_state.messages[-1]["content"]},
+        {"callbacks": [st_callback]},
+    )
+    message = {"role": "assistant", "content": response["output"]}
     st.session_state.messages.append(message)
+
+
+if len(st.session_state["messages"]) > 1:
+
+    # We set the space between the icons thanks to a share of 100
+    cols_dimensions = [7, 19.4, 19.3, 9, 8.6, 8.6, 28.1]
+    col0, col1, col2, col3, col4, col5, col6 = st.columns(cols_dimensions)
+
+    with col1:
+
+        # Converts the list of messages into a JSON Bytes format
+        json_messages = json.dumps(st.session_state["messages"]).encode("utf-8")
+
+        # And the corresponding Download button
+        st.download_button(
+            label="📥 Save chat!",
+            data=json_messages,
+            file_name="chat_conversation.json",
+            mime="application/json",
+        )
+
+    with col2:
+        st.button("Clear Chat 🧹", on_click=clear_chat_history)
+
+    with col3:
+        st.button("🔁", on_click=rerun_last_question)
+
+    with col4:
+        st.button("👍", on_click=lambda: log_feedback("👍"))
+
+    with col5:
+        st.button("👎", on_click=lambda: log_feedback("👎"))
+
+    with col6:
+
+        # We initiate a tokenizer
+        enc = tiktoken.get_encoding("cl100k_base")
+
+        # We encode the messages
+        tokenized_full_text = enc.encode(
+            " ".join([item["content"] for item in st.session_state["messages"]])
+        )
+
+        # And display the corresponding number of tokens
+        label = f"💬 {len(tokenized_full_text)} tokens"
+        st.button(label)
+
+# else:
+
+#     # At the first run of a session, we temporarly display a message
+#     if "disclaimer" not in st.session_state:
+#         with st.empty():
+#             for seconds in range(3):
+#                 st.warning(
+#                     "‎ You can click on 👍 or 👎 to provide feedback regarding the quality of responses.",
+#                     icon="💡",
+#                 )
+#                 time.sleep(1)
+#             st.write("")
+#             st.session_state["disclaimer"] = True
